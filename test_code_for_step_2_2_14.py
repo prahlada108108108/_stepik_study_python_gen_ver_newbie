5f2fdf8ba50dc5fd1745b8af5541f1a7dfe28a54
prahlada108108108 <96427983+prahlada108108108@users.noreply.github.com>
2022-10-25 12:11:47 +0300

    Create test_code_for_step_2_2_14.py

---
+++ test_code_for_step_2_2_14.py
@@ -0,0 +1,22 @@
+# put your python code here
+text1=input()
+text2=input()
+text3=input()
+print(text3)
+print(text2)
+print(text1)
+
+"""
+Напишите программу, которая считывает три строки по очереди,
+а затем выводит их в обратной последовательности, каждую на отдельной строчке.
+
+Sample Input 1:
+Hello
+it's
+me
+
+Sample Output 1:
+me
+it's
+Hello
+"""
