b760abf84913179f17f1f544b8538a3be1bbbae8
prahlada108108108 <96427983+prahlada108108108@users.noreply.github.com>
2022-10-25 10:40:22 +0300

    Create test_code_for_step_2_2_13.py

---
+++ test_code_for_step_2_2_13.py
@@ -0,0 +1,20 @@
+# put your python code here
+text1=input()
+text2=input()
+text3=input()
+print(text1)
+print(text2)
+print(text3)
+
+#Напишите программу, которая считывает три строки по очереди, а затем выводит их в той же последовательности, каждую на отдельной строчке.
+"""
+Sample Input 1:
+I was
+born
+this way
+
+Sample Output 1:
+I was
+born
+this way
+"""
