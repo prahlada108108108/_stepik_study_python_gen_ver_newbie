2fea12588971467f178d173cba72d470f261ef13
prahlada108108108 <96427983+prahlada108108108@users.noreply.github.com>
2022-10-25 10:33:45 +0300

    Create test_code_for_step_2_2_11.py

---
+++ test_code_for_step_2_2_11.py
@@ -0,0 +1,6 @@
+# put your python code here
+name = input()
+print('Привет,', name)
+
+#input will be name of person
+#as otput u need to get  "hello" (without "") then "," then space symbol then name
