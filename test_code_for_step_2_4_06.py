7956b36f666b4c26d267ad8ea6b358c8518bae54
prahlada108108108 <uztzt2ntjr44@mail.ru>
2022-10-28 12:52:23 +0300

    step 2_4_05 code

---
+++ test_code_for_step_2_4_06.py
@@ -0,0 +1,18 @@
+# put your python code here
+num1=int(input())
+num2=int(input())
+num3=int(input())
+print(num1+num2+num3)
+
+
+'''
+Напишите программу, которая считывает три целых числа и выводит на экран их сумму.
+Каждое число записано в отдельной строке.
+Sample Input 1:
+9
+11
+2
+
+Sample Output 1:
+22
+'''
