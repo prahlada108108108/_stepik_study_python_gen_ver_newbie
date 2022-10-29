a64aae6744e6a1e1d17eec8b725c92bf55bea003
prahlada108108108 <96427983+prahlada108108108@users.noreply.github.com>
2022-10-29 23:01:35 +0300

    Create test_code_for_step_2_4_10.py

---
+++ test_code_for_step_2_4_10.py
@@ -0,0 +1,19 @@
+# put your python code here
+val1=int(input())
+val2=int(input())
+val3=int(input())
+val4=int(input())
+print((val1+val2+val3+val4)*3)
+
+'''
+Напишите программу, которая считает стоимость трех компьютеров, состоящих из монитора, системного блока, клавиатуры и мыши.
+
+Sample Input 1:
+9900
+55600
+3999
+2990
+
+Sample Output 1:
+217467
+'''
