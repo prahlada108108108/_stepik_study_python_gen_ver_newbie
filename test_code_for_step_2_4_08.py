ccaa4f66628d608cf8c48a0a52397e78c2ea8d8d
prahlada108108108 <96427983+prahlada108108108@users.noreply.github.com>
2022-10-29 13:45:07 +0300

    Create test_code_for_step_2_4_08.py

---
+++ test_code_for_step_2_4_08.py
@@ -0,0 +1,16 @@
+# put your python code here
+a=int(input()) 
+b=int(input()) 
+print(3*(a+b)**3+275*b**2-127*a-41)
+
+'''
+Напишите программу вычисления значения функции f(a,b) = 3(a + b)^3 + 275b^2 - 127a - 41
+
+Sample Input 1:
+1
+1
+
+Sample Output 1:
+131
+
+'''
