2dc347720b5e71cf5f448e93e21b07db4fd0f0e7
prahlada108108108 <96427983+prahlada108108108@users.noreply.github.com>
2022-10-30 19:22:59 +0300

    Create test_code_for_step_2_4_11.py

---
+++ test_code_for_step_2_4_11.py
@@ -0,0 +1,20 @@
+# put your python code here
+num1=int(input())
+num2=int(input())
+print(num1,'+',num2,'=',num1+num2)
+print(num1,'-',num2,'=',num1-num2)
+print(num1,'*',num2,'=',num1*num2)
+
+'''
+Арифметические операции
+Напишите программу, в которой вычисляется сумма, разность и произведение двух целых чисел, введенных с клавиатуры.
+
+Sample Input 1:
+2
+7
+
+Sample Output 1:
+2 + 7 = 9
+2 - 7 = -5
+2 * 7 = 14
+'''
