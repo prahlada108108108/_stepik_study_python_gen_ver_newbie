640360b816e48cff41cba0aaa56043431cd14290
prahlada108108108 <96427983+prahlada108108108@users.noreply.github.com>
2022-11-20 08:33:27 -0800

    string concatenation

---
+++ test_code_for_step_6_2_05.py
@@ -0,0 +1,17 @@
+# put your python code here
+print('"Python is a great language!"'+', said Fred. '+'"I'+" don't"+' ever remember having this much fun before."')
+
+'''
+Напишите программу, которая выводит текст:
+
+"Python is a great language!", said Fred. "I don't ever remember having this much fun before."
+
+Примечание. Используйте конкатенацию строк.
+
+Тестовые данные 🟢
+Sample Input:
+
+Sample Output:
+
+"Python is a great language!", said Fred. "I don't ever remember having this much fun before."
+'''
