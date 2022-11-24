6bdbfa97457f0d45b745088d570373c6c85cfa4b
prahlada108108108 <96427983+prahlada108108108@users.noreply.github.com>
2022-11-24 05:16:20 -0800

    "for" cycle; print

---
+++ test_code_for_step_7_1_02.py
@@ -0,0 +1,30 @@
+# put your python code here
+
+for i in range (10):
+    print('Python is awesome!')
+
+'''
+Python is awesome
+Напишите программу, которая выводит слова «Python is awesome!» (без кавычек) 10 раз.
+
+Формат входных данных
+
+Формат выходных данных
+Программа должна вывести 10 раз текст «Python is awesome!», каждый на отдельной строке.
+
+Тестовые данные 🟢
+Sample Input:
+
+Sample Output:
+
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+'''
