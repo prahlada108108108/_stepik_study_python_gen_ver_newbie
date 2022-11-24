92d20cccf72ec46775678cec082a1daa5c54bee9
prahlada108108108 <96427983+prahlada108108108@users.noreply.github.com>
2022-11-24 05:30:40 -0800

    print multiple lines multiple time; for loop

---
+++ test_code_for_step_7_1_04.py
@@ -0,0 +1,69 @@
+# put your python code here
+for i in range (6):
+    print('AAA')
+for i in range (5):
+    print('BBBB')    
+print('E')
+for i in range (9):
+    print('TTTTT')
+print('G')    
+
+'''
+Последовательность символов
+Напишите программу, которая использует ровно три цикла for для печати следующей последовательности символов:
+
+AAA
+AAA
+AAA
+AAA
+AAA
+AAA
+BBBB
+BBBB
+BBBB
+BBBB
+BBBB
+E
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+G
+Формат входных данных
+
+Формат выходных данных
+Программа должна вывести указанную последовательность символов.
+
+Тестовые данные 🟢
+Sample Input:
+
+Sample Output:
+
+AAA
+AAA
+AAA
+AAA
+AAA
+AAA
+BBBB
+BBBB
+BBBB
+BBBB
+BBBB
+E
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+G
+'''
